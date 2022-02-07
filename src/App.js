b48f9374da3c03d86ed09dd5fbb1de8cb0c5e5e3
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-02-07 09:34:37 -0600

    added ToDoList.js, added import useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React, { useState } from "react";
 import "./App.css";
 
+import Form from "./components/Form";
+import TodoList from "./components/ToDoList";
+
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
       <header>
         <h1>To Do List</h1>
       </header>
+      <Form />
+      <TodoList />
     </div>
   );
 }
