bf0372d6bbd8a5e32ff3ad0694364623eb55a46b
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-02-07 16:25:30 -0600

    added delete and completed functionality, need to debug TypeError ToDoList.js.7

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import Form from "./components/Form";
 import TodoList from "./components/ToDoList";
 
 function App() {
+  //State
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  //use Effect to create
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+
+  //functions
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.uncompleted === true));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
   return (
     <div className="App">
       <header>
@@ -17,6 +40,8 @@ function App() {
         todos={todos}
         setTodos={setTodos}
         setInputText={setInputText}
+        setStatus={setStatus}
+        filteredTodos={filteredTodos}
       />
       <TodoList setTodos={setTodos} text={todos} />
     </div>
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,12 +1,18 @@
 import React from "react";
 //import components
 import Todo from "./Todo";
-const TodoList = ({ todos, setTodos }) => {
+const TodoList = ({ todos, setTodos, filteredTodos }) => {
   return (
     <div className="todo-container">
       <ul className="todo-list">
-        {todos.map((todo) => (
-          <Todo key={todo.id} text={todo.text} id={todo.id} />
+        {filteredTodos.map((todo) => (
+          <Todo
+            setTodos={setTodos}
+            todos={todos}
+            key={todo.id}
+            todo={todo}
+            text={todo.text}
+          />
         ))}
       </ul>
     </div>
